0f2063294f081c6ea5765720a5a04ee8e9a192f0
kms80 <35785769+kms80@users.noreply.github.com>
2018-03-30 12:12:47 -0400

    Edited for Lab4.3

--- RBE 2002 Lab_03 - Velocity_controller/Lab_3_RBE2002_Velocity_controller/PID.cpp
+++ RBE 2002 Lab_03 - Velocity_controller/Lab_3_RBE2002_Velocity_controller/PID.cpp
@@ -9,7 +9,7 @@
 
 //Class constructor
 PID::PID(){
-  
+  index = 0;
 }
 
 //Function to set PID gain values
@@ -19,22 +19,47 @@ void PID::setpid(float P, float I, float D){
   kd=D;
 }
 
+static double sum(double a[], int s){
+//a = array, s = size
+  double total = 0.0; 
+  for (int x = 0; x < s; x++){
+    total += a[x];
+  }
+  return total;
+}
+
 //Write this function to calculate a control signal from the set velocity 
 //and the current velocity 
 float PID::calc(double setVel, double curVel){
 
     // calculate error
-
+    double error = setVel - curVel; 
+        
     // calculate derivative of error
-
-    // calculate integral error
-
+    double derivative = error - lasterror; 
+    lasterror = error; 
+    
+    // calculate integral error. Running average is best but hard to implement
+    pasterror[index] = error; 
+    index = (index+1)%ARRAY_SIZE; 
+    double integral = sum(pasterror, ARRAY_SIZE); 
 
     // sum up the error value to send to the motor based off gain values. 
- 
- 
+    float control_signal = (kp*error + ki*integral + kd*derivative);
+    Serial.print("Kp value:");
+    Serial.println(kp*error);
+    Serial.print("Ki value:");
+    Serial.println(ki*integral);
+    Serial.print("Kd value:");
+    Serial.println(kd*derivative);
+    
     // limit control value to 0-254
-
+    if (control_signal > 254){
+      control_signal = 254; 
+    }else if (control_signal < 0){
+      control_signal = 0;
+    }
+ 
     //return the control signal
-    return control_input;
+    return control_signal;
 }
